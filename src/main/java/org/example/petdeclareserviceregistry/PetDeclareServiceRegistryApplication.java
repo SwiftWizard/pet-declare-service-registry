eb0e895bebec866d7f50ace897376ed7e2cbb964
Ruben <kovacevic.ruben@gmail.com>
2024-06-07 15:34:14 +0200

    init: Initial commit, service registry for pet.declare project

---
+++ src/main/java/org/example/petdeclareserviceregistry/PetDeclareServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package org.example.petdeclareserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetDeclareServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetDeclareServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/petdeclareserviceregistry/PetDeclareServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.petdeclareserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetDeclareServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
